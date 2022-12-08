7f258cde73c4de9335155f01ab2883bf09e72482
aakj2010 <aakj2010@gmail.com>
2022-12-08 12:55:05 +0530

    Final Commit

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv").config()
 
+const URL = process.env.DB;
 const connectDB = async () => {
+   
     try {
-        const conn = await mongoose.connect('mongodb+srv://Arslan:admin123@cluster0.jiog1sg.mongodb.net/myurlShortener?retryWrites=true&w=majority')
+        // const conn = await mongoose.connect('mongodb+srv://Arslan:admin123@cluster0.jiog1sg.mongodb.net/myurlShortener?retryWrites=true&w=majority')
+        const conn = await mongoose.connect(URL)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
